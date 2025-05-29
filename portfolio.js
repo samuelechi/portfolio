ba08c606425bf58758f7b44bb71e31b495ebdca1
samuelechi <edomebube@yahoo.com>
2025-05-29 18:25:02 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -60,25 +60,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll("nav a");
+const navLinks = document.querySelectorAll(".menu-ul li a");
 
-  window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop - 100;
-      if (pageYOffset >= sectionTop) {
-        current = section.getAttribute("id");
-      }
-    });
-
-    navLinks.forEach((link) => {
-      link.classList.remove("active");
-      if (link.getAttribute("href") === "#" + current) {
-        link.classList.add("active");
-      }
-    });
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    // Remove active from all links
+    navLinks.forEach(nav => nav.classList.remove("active-link"));
+    // Add active to clicked link
+    link.classList.add("active-link");
   });
+});
 
 
 const menubtn = document.querySelector(".menu-btn");
